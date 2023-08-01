2052ae5add264195ecc87bdb3597436ec0538666
Protsak Petro <protsakpetro@gmail.com>
2023-08-01 14:47:21 +0200

    other_version

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,22 @@
-import VimeoPlayer from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-const iframe = document.querySelector('#vimeo-player');
-const player = new VimeoPlayer(iframe, {
-  id: 236203659,
-  width: 640,
-  height: 360,
-  url: 'https://player.vimeo.com/video/236203659',
-  autoplay: true,
-});
 
-player.on(
-  'timeupdate',
-  throttle(function (data) {
-    currentTime = data.seconds;
-    const savedCurrentTimeMarkup = localStorage.setItem(
-      'videoplayer-current-time',
-      currentTime.toString()
-    );
-  }, 1000)
-);
-function getSavedCurrentTime() {
-  const savedCurrentTime = localStorage.getItem(
-    'videoplayer-current-time' || 0
-  );
-  if (savedCurrentTime) {
-    return Number(savedCurrentTime);
-  }
-}
+const player = new Player('vimeo-player');
 
-player.setCurrentTime(getSavedCurrentTime());
+const onTime = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+player.on('timeupdate', throttle(onTime, 1000));
