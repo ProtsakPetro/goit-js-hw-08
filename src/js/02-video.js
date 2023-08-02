6aa14c5c45d054f1d78b27f45b83b897c7e62c7a
Protsak Petro <protsakpetro@gmail.com>
2023-08-02 21:08:25 +0200

    mentor_advise

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,16 +7,20 @@ const onTime = function (data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
-player
-  .setCurrentTime(currentTime)
-  .then(function (seconds) {})
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        break;
 
-      default:
-        break;
-    }
-  });
+//* замість коду нижче додав player.setCurrentTime(currentTime || 0);
+//player
+//   .setCurrentTime(currentTime)
+//   .then(function (seconds) {})
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         break;
+
+//       default:
+//         break;
+//     }
+//   });
+//*оптимізував код як порадив ментор
+player.setCurrentTime(currentTime || 0);
 player.on('timeupdate', throttle(onTime, 1000));
